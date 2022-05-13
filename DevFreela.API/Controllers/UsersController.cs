1e3fef15a1f68113f308ae49719ee4849bb44ea3
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-12 22:05:20 -0300

    Add files via upload

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using DevFreela.API.Models;
+using DevFreela.Application.Commands.CreateUser;
+using DevFreela.Application.Queries.GetUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // api/users/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetUserQuery(id);
+
+            var user = await _mediator.Send(query);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // api/users
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateUserCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, command);
+        }
+
+        // api/users/1/login
+        [HttpPut("{id}/login")]
+        public IActionResult Login(int id, [FromBody] LoginModel login)
+        {
+            // TODO: Para Módulo de Autenticação e Autorização
+
+            return NoContent();
+        }
+    }
+}
